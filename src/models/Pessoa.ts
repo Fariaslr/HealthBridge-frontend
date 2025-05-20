9ed82ae2a8643a6943c08bc44a862e4766488ac2
Fariaslr <lucasfarias485@gmail.com>
2025-05-19 21:30:33 -0300

    Criando o type Pessoa para autenticação :hammer:

---
+++ src/models/Pessoa.ts
@@ -0,0 +1,20 @@
+export type Sexo = "MASCULINO" | "FEMININO" | "OUTRO"; // baseado no seu Enum em Java
+
+export type Endereco = {
+  cep: string;
+  complemento: string;
+};
+
+export type Pessoa = {
+  id: string;
+  cpf: string;
+  nome: string;
+  sobrenome: string;
+  telefone: string;
+  usuario: string;
+  email: string;
+  senha?: string; // opcional para evitar expor desnecessariamente
+  dataNascimento: string; // será uma string ISO ao vir do backend
+  sexo: Sexo;
+  endereco: Endereco;
+};
